d5da6b34bb329b0cd75b69046f9b6943fc9ee1c9
Trajectoires <v.gillibert@reseau-apis.fr>
2017-09-25 14:28:26 +0200

    Pass server http to expressjs
    + expressjs server
    + static folder

--- server.js
+++ server.js
@@ -1,25 +1,31 @@
-var app = require('http').createServer(handler),
-     io = require('socket.io').listen(app),
+var express = require('express'),
+    app = express(),
+    server = require('http').Server(app),
+     io = require('socket.io').listen(server),
      fs = require('fs'),
-   five = require('johnny-five'),
-   board = new five.Board();
+     five = require('johnny-five'),
+     board = new five.Board();
 
-app.listen(8080);
+// Ecoute sur le port 8080 de localhost
+server.listen(8080);
 
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
+// Création du server expressJS
+app.get('/',function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile(__dirname + '/index.html', function (err,data) {
+      if (err) {
+        res.writeHead(500);
+        return res.end('Error loading index.html');
+      };
+      res.end(data);
+    });
+});
 
-    res.writeHead(200);
-    res.end(data);
-  });
-}
+// Création du dossier statique /public
+app.use('/public', express.static(__dirname + '/public'));
 
-board.on("ready", function() {
+// Manipulation de la carte Arduino avec johnny-five
+board.on('ready', function() {
   // Create a new `led` instance.
   var led = new five.Led(13);
   // Create a new `button` hardware instance.
@@ -28,13 +34,13 @@ board.on("ready", function() {
     isPullup: true,
   });
 
-  io.sockets.on("connection", function (socket) {
+  io.on('connection', function (socket) {
     socket.on('click', function () {
       led.toggle();
     });
   });
 
-  button.on("press", function() {
+  button.on('press', function() {
     console.log( "Button pressed" );
     led.toggle();
   });
