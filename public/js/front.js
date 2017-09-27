b821ad4ed11fb5741ebcc89f467327f1180602f4
Trajectoires <v.gillibert@reseau-apis.fr>
2017-09-27 15:22:05 +0200

    Ajout svg dans html
    + ajout de la carte dans le html
    + Débug du lancement JQuery audio

--- public/js/front.js
+++ public/js/front.js
@@ -1,15 +1,16 @@
 $(document).ready(function() {
-  var socket = io.connect('http://localhost:8080');
+  var socket = io('http://localhost:8080');
+  var espagnol = new Audio("http://localhost:8080/public/sons/espagnol.mp3");
+  var vietnamien = new Audio("http://localhost:8080/public/sons/vietnamien.mp3");
 
-  //click sur le bouton
-  $('#button').click(function(e){
+  //click sur le pays
+  $('#espagne').click(function(e){
     socket.emit('click');
     e.preventDefault();
   });
 
-  io.on('connection', function (socket) {
-    socket.on('song', function () {
-      $('#espagnol')[0].play();
-    });
+  //click sur le bouton
+  socket.on('song', function () {
+    espagnol.play();
   });
 });
