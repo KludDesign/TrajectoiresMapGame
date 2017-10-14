8829d59bbf50a29f3c4b82ed6a24285e4c657504
KludDesign <vincentgillibert@gmail.com>
2017-10-14 21:11:26 +0200

    Rapatriement server / front
    + Rapatriement des fonctions de traitement depuis server vers front
    + Automatisation des fonctions

--- public/js/front.js
+++ public/js/front.js
@@ -1,262 +1,73 @@
 $(document).ready( () => {
   var socket = io('http://localhost:8080');
-  var espagnol = new Audio("http://localhost:8080/public/sons/espagnol.mp3");
-  var vietnamien = new Audio("http://localhost:8080/public/sons/vietnamien.mp3");
-  var japonais = new Audio("http://localhost:8080/public/sons/japonais.mp3");
+  var espagne = new Audio("http://localhost:8080/public/sons/espagnol.mp3");
+  var vietnam = new Audio("http://localhost:8080/public/sons/vietnamien.mp3");
+  var japon = new Audio("http://localhost:8080/public/sons/japonais.mp3");
 
-  $('#faux').hide();
-  $('#danemarkTexte').hide();
-  $('#espagneTexte').hide();
-  $('#espagneDTexte').hide();
-  $('#franceTexte').hide();
-  $('#royaumeUniTexte').hide();
-  $('#portugalTexte').hide();
-  $('#paysBasTexte').hide();
-  $('#canadaTexte').hide();
-  $('#indeTexte').hide();
-  $('#mexiqueTexte').hide();
-  $('#norvegeTexte').hide();
-  $('#vietnamTexte').hide();
-  $('#japonTexte').hide();
-  $('#greceTexte').hide();
-  $('#moyenOrientTexte').hide();
   $('#appui').show();
 
+  $('.pays').each(function() {
+    $(this).click( () => {
+      var id = $(this).attr('id');
+      $('text').hide();
+      $('#'+id+'Texte').show();
+    });
+  });
+
+  //Récupération du boutton
+  // $('.pays').each(function() {
+  //   var id = $(this).attr('id');
+  //   socket.on(id+'Button', (id) => {
+  //     return bouton = id;
+  //   });
+  // });
+
+  //click sur les boutons pour le jeu de Droite
+  socket.on('espagneSong', () => {
+    espagne.play();
+  });
+
+  socket.on('vietnamSong', () => {
+    vietnam.play();
+  });
+
+  socket.on('japonSong', () => {
+    japon.play();
+  });
+
+
+
+
+
+  //Pour la mise a zero lors de l'appui sur un bouton
   socket.on('miseZero', () => {
     $('#faux').hide();
-    $('#danemarkTexte').hide();
-    $('#espagneTexte').hide();
-    $('#espagneDTexte').hide();
-    $('#franceTexte').hide();
-    $('#royaumeUniTexte').hide();
-    $('#portugalTexte').hide();
-    $('#paysBasTexte').hide();
-    $('#canadaTexte').hide();
-    $('#indeTexte').hide();
-    $('#mexiqueTexte').hide();
-    $('#norvegeTexte').hide();
-    $('#vietnamTexte').hide();
-    $('#japonTexte').hide();
-    $('#greceTexte').hide();
-    $('#moyenOrientTexte').hide();
     $('#appui').hide();
+    $('.pays').each(function() {
+      var id = $(this).attr('id');
+      $('#'+id+'Texte').hide();
+    });
   });
 
-  //Affichage du texte
-  socket.on('faux', () => {
-    $('#faux').show(500);
-    $('#danemarkTexte').hide();
-    $('#espagneTexte').hide();
-    $('#espagneDTexte').hide();
-    $('#franceTexte').hide();
-    $('#royaumeUniTexte').hide();
-    $('#portugalTexte').hide();
-    $('#paysBasTexte').hide();
-    $('#canadaTexte').hide();
-    $('#indeTexte').hide();
-    $('#mexiqueTexte').hide();
-    $('#norvegeTexte').hide();
-    $('#vietnamTexte').hide();
-    $('#japonTexte').hide();
-    $('#greceTexte').hide();
-    $('#moyenOrientTexte').hide();
-      $('#appui').hide();
-  });
-
-  socket.on('danemarkValid', () => {
-    $('#danemarkTexte').show(500);
-    $('#faux').hide();
-  });
-
-  socket.on('espagneValid', () => {
-    $('#espagneTexte').show(500);
-    $('#faux').hide();
-  });
-
-  socket.on('espagneDValid', () => {
-    $('#espagneDTexte').show(500);
-    $('#faux').hide();
-  });
-
-  socket.on('franceValid', () => {
-    $('#franceTexte').show(500);
-    $('#faux').hide();
-  });
-
-  socket.on('royaumeUniValid', () => {
-    $('#royaumeUniTexte').show(500);
-    $('#faux').hide();
-  });
-
-  socket.on('portugalValid', () => {
-    $('#portugalTexte').show(500);
-    $('#faux').hide();
-  });
-
-  socket.on('paysBasValid', () => {
-    $('#paysBasTexte').show(500);
-    $('#faux').hide();
-  });
-
-  socket.on('canadaValid', () => {
-    $('#canadaTexte').show(500);
-    $('#faux').hide();
-  });
-
-  socket.on('indeValid', () => {
-    $('#indeTexte').show(500);
-    $('#faux').hide();
-  });
-
-  socket.on('mexiqueValid', () => {
-    $('#mexiqueTexte').show(500);
-    $('#faux').hide();
-  });
-
-  socket.on('norvegeValid', () => {
-    $('#norvegeTexte').show(500);
-    $('#faux').hide();
-  });
-
-  socket.on('vietnamValid', () => {
-    $('#vietnamTexte').show(500);
-    $('#faux').hide();
-  });
-
-  socket.on('japonValid', () => {
-    $('#japonTexte').show(500);
-    $('#faux').hide();
-  });
-
-  socket.on('greceValid', () => {
-    $('#greceTexte').show(500);
-    $('#faux').hide();
-  });
-
-  socket.on('moyenOrientValid', () => {
-    $('#moyenOrientTexte').show(500);
-    $('#faux').hide();
-  });
-
-  //click sur le pays
-  $('#espagne').click( (e) => {
-    socket.emit('clickEspagne');
-    e.preventDefault();
-  });
-
-  $('#danemark').click( (e) => {
-    socket.emit('clickDanemark');
-    e.preventDefault();
-  });
-
-  $('#france').click( (e) => {
-    socket.emit('clickFrance');
-    e.preventDefault();
-  });
-
-  $('#royaumeUni').click( (e) => {
-    socket.emit('clickRoyaumeUni');
-    e.preventDefault();
-  });
-
-  $('#portugal').click( (e) => {
-    socket.emit('clickPortugal');
-    e.preventDefault();
-  });
-
-  $('#paysBas').click( (e) => {
-    socket.emit('clickPaysBas');
-    e.preventDefault();
-  });
-
-  $('#canada').click( (e) => {
-    socket.emit('clickCanada');
-    e.preventDefault();
-  });
-
-  $('#inde').click( (e) => {
-    socket.emit('clickInde');
-    e.preventDefault();
-  });
-
-  $('#mexique').click( (e) => {
-    socket.emit('clickMexique');
-    e.preventDefault();
-  });
-
-  $('#norvege').click( (e) => {
-    socket.emit('clickNorvege');
-    e.preventDefault();
-  });
-
-  $('#vietnam').click( (e) => {
-    socket.emit('clickVietnam');
-    e.preventDefault();
-  });
-
-  $('#japon').click( (e) => {
-    socket.emit('clickJapon');
-    e.preventDefault();
-  });
-
-  $('#grece').click( (e) => {
-    socket.emit('clickGrece');
-    e.preventDefault();
-  });
-
-  $('#moyenOrient').click( (e) => {
-    socket.emit('clickMoyenOrient');
-    e.preventDefault();
-  });
-
-  $('#autre').click( (e) => {
-    socket.emit('clickAutre');
-    e.preventDefault();
-  });
-
-  //click sur le bouton pour lancer le son
-  socket.on('songEspagne', () => {
-    espagnol.play();
-  });
-
-  socket.on('songJapon', () => {
-    japonais.play();
-  });
-
-  socket.on('songGrece', () => {
-    espagnol.play();
-  });
-
-  socket.on('songVietnam', () => {
-    vietnamien.play();
-  });
-
-  socket.on('songNorvege', () => {
-    espagnol.play();
-  });
+  // //lorsque c'est faux
+  // socket.on('faux', () => {
+  //   $('#faux').show(500);
+  //   $('#appui').hide();
+  //   $('.pays').each(function() {
+  //     var id = $(this).attr('id');
+  //     $('#'+id+'Texte').hide();
+  //   });
+  // });
 
   //click sur le bouton pour changer de jeu (change la couleur de la map)
   socket.on('swichJeu', () => {
     $('.map').toggleClass('map2');
 
     $('#faux').hide();
-    $('#danemarkTexte').hide();
-    $('#espagneTexte').hide();
-    $('#espagneDTexte').hide();
-    $('#franceTexte').hide();
-    $('#royaumeUniTexte').hide();
-    $('#portugalTexte').hide();
-    $('#paysBasTexte').hide();
-    $('#canadaTexte').hide();
-    $('#indeTexte').hide();
-    $('#mexiqueTexte').hide();
-    $('#norvegeTexte').hide();
-    $('#vietnamTexte').hide();
-    $('#japonTexte').hide();
-    $('#greceTexte').hide();
-    $('#moyenOrientTexte').hide();
     $('#appui').show();
+    $('.pays').each(function() {
+      var id = $(this).attr('id');
+      $('#'+id+'Texte').hide();
+    });
   });
-
-
 });
--- server.js
+++ server.js
@@ -7,10 +7,7 @@ var express = require('express'),
      eventEmitter = new events.EventEmitter(),
      // johnny-five
      five = require('johnny-five'),
-     board = new five.Board(),
-
-     mapArray = {espagneMap: false, espagneDMap: false, danemarkMap: false, franceMap: false, royaumeUniMap: false, portugalMap: false, paysBasMap: false, canadaMap: false, indeMap: false, mexiqueMap: false, norvegeMap: false, VietnamMap: false, japonMap: false, greceMap: false, moyenOrientMap: false, autreMap: false},
-     buttonArray = {espagneButton: false, espagneDButton: false, indeButton: false, danemarkButton: false, franceButton: false, royaumeUniButton: false, portugalButton: false, paysBasButton: false, canadaButton: false, mexiqueButton: false, norvegeButton: false, VietnamButton: false, japonButton: false, greceButton: false, moyenOrientButton: false};
+     board = new five.Board();
 
 // Ecoute sur le port 8080 de localhost
 server.listen(8080);
@@ -34,13 +31,13 @@ app.use('/public', express.static(__dirname + '/public'));
 board.on('ready', () => {
   // Déclaration des bouttons jeu de gauche ------------------------------------
   var buttonsG = new five.Buttons({
-    pins: [44, 45, 46, 47, 48, 49, 50, 51, 52, 53],
+    pins: [10],
     isPullup: true
   });
 
   // Déclaration des boutons jeu de droite ----------------------------------
   var buttonsD = new five.Buttons({
-    pins: [8, 9, 10, 11, 12],
+    pins: [8],
     isPullup: true
   });
 
@@ -50,7 +47,7 @@ board.on('ready', () => {
   });
 
   var relay = new five.Relay({
-    pin: 21,
+    pin: 2,
     type: "NO",
   });
 
@@ -65,212 +62,14 @@ board.on('ready', () => {
   // Evenement lors du click sur la map ------------------------------------------------------
   io.on('connection', (socket) => {
 
-    socket.on('clickEspagne',  () => {
+    socket.on('japonClick',  () => {
       console.log( "clickEspagne" );
-      mapArray.espagneMap = true;
-      if (mapArray.espagneMap == true && buttonArray.espagneButton == true){
-        io.emit('espagneValid');
-        console.log( "gagné!!!" );
-      }
-      if (mapArray.espagneMap == true && buttonArray.espagneDButton == true){
-        io.emit('espagneDValid');
-        console.log( "gagné!!!" );
-      }
-      else if(mapArray.espagneMap == false || buttonArray.espagneButton == false){
-        console.log( "tu t'es trompé!!");
-        io.emit('faux');
-      };
-      return mapArray.espagneMap = false, buttonArray.espagneButton = false;
+      io.emit('japonButton');
     });
 
-    socket.on('clickJapon',  () => {
-      console.log( "clickJapon" );
-      mapArray.japonMap = true;
-      if (mapArray.japonMap == true && buttonArray.japonButton == true){
-        io.emit('japonValid');
-        console.log( "gagné!!!" );
-      }
-      else if(mapArray.japonMap == false || buttonArray.japonButton == false){
-        console.log( "tu t'es trompé!!");
-        io.emit('faux');
-      };
-      return mapArray.japonMap = false, buttonArray.japonButton = false;
-    });
-
-    socket.on('clickGrece',  () => {
-      console.log( "clickGrece" );
-      mapArray.greceMap = true;
-      if (mapArray.greceMap == true && buttonArray.greceButton == true){
-        io.emit('greceValid');
-        console.log( "gagné!!!" );
-      }
-      else if(mapArray.greceMap == false || buttonArray.greceButton == false){
-        console.log( "tu t'es trompé!!");
-        io.emit('faux');
-      };
-      return mapArray.greceMap = false, buttonArray.greceButton = false;
-    });
-
-    socket.on('clickVietnam',  () => {
-      console.log( "clickVietnam" );
-      mapArray.vietnamMap = true;
-      if (mapArray.vietnamMap == true && buttonArray.vietnamButton == true){
-        io.emit('vietnamValid');
-        console.log( "gagné!!!" );
-      }
-      else if(mapArray.vietnamMap == false || buttonArray.vietnamButton == false){
-        console.log( "tu t'es trompé!!");
-        io.emit('faux');
-      };
-      return mapArray.vietnamMap = false, buttonArray.vietnamButton = false;
-    });
-
-    socket.on('clickNorvege',  () => {
-      console.log( "clickNorvege" );
-      mapArray.norvegeMap = true;
-      if (mapArray.norvegeMap == true && buttonArray.norvegeButton == true){
-        io.emit('norvegeValid');
-        console.log( "gagné!!!" );
-      }
-      else if(mapArray.norvegeMap == false || buttonArray.norvegeButton == false){
-        console.log( "tu t'es trompé!!");
-        io.emit('faux');
-      };
-      return mapArray.norvegeMap = false, buttonArray.norvegeButton = false;
-    });
-
-    socket.on('clickFrance',  () => {
+    socket.on('indeClick',  () => {
       console.log( "clickFrance" );
-      mapArray.franceMap = true;
-      if (mapArray.franceMap == true && buttonArray.franceButton == true){
-        io.emit('franceValid');
-        console.log( "gagné!!!" );
-      }
-      else if(mapArray.franceMap == false || buttonArray.franceButton == false){
-        console.log( "tu t'es trompé!!");
-        io.emit('faux');
-      };
-      return mapArray.franceMap = false, buttonArray.franceButton = false;
-    });
-
-    socket.on('clickDanemark',  () => {
-      console.log( "clickDanemark" );
-      mapArray.danemarkMap = true;
-      if (mapArray.danemarkMap == true && buttonArray.danemarkButton == true){
-        io.emit('danemarkValid');
-        console.log( "gagné!!!" );
-      }
-      else if(mapArray.danemarkMap == false || buttonArray.danemarkButton == false){
-        console.log( "tu t'es trompé!!");
-        io.emit('faux');
-      };
-      return mapArray.danemarkMap = false, buttonArray.danemarkButton = false;
-    });
-
-    socket.on('clickRoyaumeUni',  () => {
-      console.log( "clickRoyaumeUni" );
-      mapArray.royaumeUniMap = true;
-      if (mapArray.royaumeUniMap == true && buttonArray.royaumeUniButton == true){
-        io.emit('royaumeUniValid');
-        console.log( "gagné!!!" );
-      }
-      else if(mapArray.royaumeUniMap == false || buttonArray.royaumeUniButton == false){
-        console.log( "tu t'es trompé!!");
-        io.emit('faux');
-      };
-      return mapArray.royaumeUniMap = false, buttonArray.royaumeUniButton = false;
-    });
-
-    socket.on('clickPortugal',  () => {
-      console.log( "clickPortugal" );
-      mapArray.portugalMap = true;
-      if (mapArray.portugalMap == true && buttonArray.portugalButton == true){
-        io.emit('portugalValid');
-        console.log( "gagné!!!" );
-      }
-      else if(mapArray.portugalMap == false || buttonArray.portugalButton == false){
-        console.log( "tu t'es trompé!!");
-        io.emit('faux');
-      };
-      return mapArray.portugalMap = false, buttonArray.portugalButton = false;
-    });
-
-    socket.on('clickPaysBas',  () => {
-      console.log( "clickPaysBas" );
-      mapArray.paysBasMap = true;
-      if (mapArray.paysBasMap == true && buttonArray.paysBasButton == true){
-        io.emit('paysBasValid');
-        console.log( "gagné!!!" );
-      }
-      else if(mapArray.paysBasMap == false || buttonArray.paysBasButton == false){
-        console.log( "tu t'es trompé!!");
-        io.emit('faux');
-      };
-      return mapArray.paysBasMap = false, buttonArray.paysBasButton = false;
-    });
-
-    socket.on('clickCanada',  () => {
-      console.log( "clickCanada" );
-      mapArray.canadaMap = true;
-      if (mapArray.canadaMap == true && buttonArray.canadaButton == true){
-        io.emit('canadaValid');
-        console.log( "gagné!!!" );
-      }
-      else if(mapArray.canadaMap == false || buttonArray.canadaButton == false){
-        console.log( "tu t'es trompé!!");
-        io.emit('faux');
-      };
-      return mapArray.canadaMap = false, buttonArray.canadaButton = false;
-    });
-
-    socket.on('clickInde',  () => {
-      console.log( "clickInde" );
-      mapArray.indeMap = true;
-      if (mapArray.indeMap == true && buttonArray.indeButton == true){
-        io.emit('indeValid');
-        console.log( "gagné!!!" );
-      }
-      else if(mapArray.indeMap == false || buttonArray.indeButton == false){
-        console.log( "tu t'es trompé!!");
-        io.emit('faux');
-      };
-      return mapArray.indeMap = false, buttonArray.indeButton = false;
-    });
-
-    socket.on('clickMexique',  () => {
-      console.log( "clickMexique" );
-      mapArray.mexiqueMap = true;
-      if (mapArray.mexiqueMap == true && buttonArray.mexiqueButton == true){
-        io.emit('mexiqueValid');
-        console.log( "gagné!!!" );
-      }
-      else if(mapArray.mexiqueMap == false || buttonArray.mexiqueButton == false){
-        console.log( "tu t'es trompé!!");
-        io.emit('faux');
-      };
-      return mapArray.mexiqueMap = false, buttonArray.mexiqueButton = false;
-    });
-
-    socket.on('clickMoyenOrient',  () => {
-      console.log( "clickMoyenOrient" );
-      mapArray.moyenOrientMap = true;
-      if (mapArray.moyenOrientMap == true && buttonArray.moyenOrientButton == true){
-        io.emit('moyenOrientValid');
-        console.log( "gagné!!!" );
-      }
-      else if(mapArray.moyenOrientMap == false || buttonArray.moyenOrientButton == false){
-        console.log( "tu t'es trompé!!");
-        io.emit('faux');
-      };
-      return mapArray.moyenOrientMap = false, buttonArray.moyenOrientButton = false;
-    });
-
-    socket.on('clickAutre',  () => {
-      if(buttonArray.indeButton == true || buttonArray.danemarkButton == true || buttonArray.franceButton == true || buttonArray.royaumeUniButton == true || buttonArray.portugalButton == true || buttonArray.paysBasButton == true || buttonArray.canadaButton == true || buttonArray.mexiqueButton == true || buttonArray.norvegeButton == true || buttonArray.VietnamButton == true
-         || buttonArray.japonButton == true || buttonArray.greceButton == true || buttonArray.moyenOrientButton == true || buttonArray.espagneButton == true || buttonArray.espagneDButton == true){
-           console.log( "tu t'es trompé!!");
-        io.emit('faux');
-      };
+      io.emit('indeButton');
     });
   });
 
@@ -282,104 +81,85 @@ board.on('ready', () => {
     switch (pinNum) {
       case 44:
         console.log('pays'+pinNum);
-        return buttonArray.espagneButton = true,
-        buttonArray.indeButton = buttonArray.danemarkButton = buttonArray.franceButton = buttonArray.royaumeUniButton = buttonArray.portugalButton = buttonArray.paysBasButton = buttonArray.canadaButton = buttonArray.mexiqueButton = buttonArray.norvegeButton = buttonArray.VietnamButton = buttonArray.japonButton = buttonArray.greceButton = buttonArray.moyenOrientButton = buttonArray.espagneDButton = false;
+        io.emit('espagneButton');
       break;
 
       case 45:
           console.log('pays'+pinNum);
-          return buttonArray.indeButton = true,
-          buttonArray.espagneButton = buttonArray.danemarkButton = buttonArray.franceButton = buttonArray.royaumeUniButton = buttonArray.portugalButton = buttonArray.paysBasButton = buttonArray.canadaButton = buttonArray.mexiqueButton = buttonArray.norvegeButton = buttonArray.VietnamButton = buttonArray.japonButton = buttonArray.greceButton = buttonArray.moyenOrientButton = buttonArray.espagneDButton = false;
+          io.emit('indeButton');
       break;
 
       case 46:
           console.log('pays'+pinNum);
-          return buttonArray.mexiqueButton = true,
-          buttonArray.espagneButton = buttonArray.indeButton = buttonArray.danemarkButton = buttonArray.franceButton = buttonArray.royaumeUniButton = buttonArray.portugalButton = buttonArray.paysBasButton = buttonArray.canadaButton = buttonArray.norvegeButton = buttonArray.VietnamButton = buttonArray.japonButton = buttonArray.greceButton = buttonArray.moyenOrientButton = buttonArray.espagneDButton = false;
+          io.emit('mexiqueButton');
       break;
 
       case 47:
           console.log('pays'+pinNum);
-          return buttonArray.paysBasButton = true,
-          buttonArray.espagneButton = buttonArray.indeButton = buttonArray.danemarkButton = buttonArray.franceButton = buttonArray.royaumeUniButton = buttonArray.portugalButton = buttonArray.canadaButton = buttonArray.mexiqueButton = buttonArray.norvegeButton = buttonArray.VietnamButton = buttonArray.japonButton = buttonArray.greceButton = buttonArray.moyenOrientButton = buttonArray.espagneDButton = false;
+          io.emit('paysBasButton');
       break;
 
       case 48:
           console.log('pays'+pinNum);
-          return buttonArray.moyenOrientButton = true,
-          buttonArray.espagneButton = buttonArray.indeButton = buttonArray.danemarkButton = buttonArray.franceButton = buttonArray.royaumeUniButton = buttonArray.portugalButton = buttonArray.paysBasButton = buttonArray.canadaButton = buttonArray.mexiqueButton = buttonArray.norvegeButton = buttonArray.VietnamButton = buttonArray.japonButton = buttonArray.greceButton = buttonArray.espagneDButton = false;
+          io.emit('moyenOrientButton');
       break;
 
       case 49:
           console.log('pays'+pinNum);
-          return buttonArray.franceButton = true,
-          buttonArray.espagneButton = buttonArray.indeButton = buttonArray.danemarkButton = buttonArray.royaumeUniButton = buttonArray.portugalButton = buttonArray.paysBasButton = buttonArray.canadaButton = buttonArray.mexiqueButton = buttonArray.norvegeButton = buttonArray.VietnamButton = buttonArray.japonButton = buttonArray.greceButton = buttonArray.moyenOrientButton = buttonArray.espagneDButton = false;
+          io.emit('franceButton');
       break;
 
       case 50:
           console.log('pays'+pinNum);
-          return buttonArray.royaumeUniButton = true,
-          buttonArray.espagneButton = buttonArray.indeButton = buttonArray.danemarkButton = buttonArray.franceButton = buttonArray.portugalButton = buttonArray.paysBasButton = buttonArray.canadaButton = buttonArray.mexiqueButton = buttonArray.norvegeButton = buttonArray.VietnamButton = buttonArray.japonButton = buttonArray.greceButton = buttonArray.moyenOrientButton = buttonArray.espagneDButton = false;
+          io.emit('royaumeUniButton');
       break;
 
       case 51:
           console.log('pays'+pinNum);
-          return buttonArray.canadaButton = true,
-          buttonArray.espagneButton = buttonArray.indeButton = buttonArray.danemarkButton = buttonArray.franceButton = buttonArray.royaumeUniButton = buttonArray.portugalButton = buttonArray.paysBasButton = buttonArray.mexiqueButton = buttonArray.norvegeButton = buttonArray.VietnamButton = buttonArray.japonButton = buttonArray.greceButton = buttonArray.moyenOrientButton = buttonArray.espagneDButton = false;
+          io.emit('canadaButton');
       break;
 
       case 52:
           console.log('pays'+pinNum);
-          return buttonArray.portugalButton = true,
-          buttonArray.espagneButton = buttonArray.indeButton = buttonArray.danemarkButton = buttonArray.franceButton = buttonArray.royaumeUniButton = buttonArray.paysBasButton = buttonArray.canadaButton = buttonArray.mexiqueButton = buttonArray.norvegeButton = buttonArray.VietnamButton = buttonArray.japonButton = buttonArray.greceButton = buttonArray.moyenOrientButton = buttonArray.espagneDButton = false;
+          io.emit('portugalButton');
       break;
 
       case 53:
           console.log('pays'+pinNum);
-          return buttonArray.danemarkButton = true,
-          buttonArray.espagneButton = buttonArray.indeButton = buttonArray.franceButton = buttonArray.royaumeUniButton = buttonArray.portugalButton = buttonArray.paysBasButton = buttonArray.canadaButton = buttonArray.mexiqueButton = buttonArray.norvegeButton = buttonArray.VietnamButton = buttonArray.japonButton = buttonArray.greceButton = buttonArray.moyenOrientButton = buttonArray.espagneDButton = false;
+          io.emit('danemarkButton');
       break;
     };
   });
 
   buttonsD.on('press', (button) => {
     var pinNum = button.pin;
+    io.emit('miseZero');
 
     switch (pinNum) {
       case 8:
         console.log('pays'+pinNum);
         // Envoyer l'événement au front lorsque le bouton est appuié
-        io.emit('songEspagne');
-        return buttonArray.espagneDButton = true,
-        buttonArray.espagneButton = buttonArray.indeButton = buttonArray.danemarkButton = buttonArray.franceButton = buttonArray.royaumeUniButton = buttonArray.portugalButton = buttonArray.paysBasButton = buttonArray.canadaButton = buttonArray.mexiqueButton = buttonArray.norvegeButton = buttonArray.VietnamButton = buttonArray.japonButton = buttonArray.greceButton = buttonArray.moyenOrientButton = false;
+        io.emit('espagneSong');
       break;
 
       case 9:
         console.log('pays'+pinNum);
-        io.emit('songJapon');
-        return buttonArray.japonButton = true,
-        buttonArray.espagneButton = buttonArray.indeButton = buttonArray.danemarkButton = buttonArray.franceButton = buttonArray.royaumeUniButton = buttonArray.portugalButton = buttonArray.paysBasButton = buttonArray.canadaButton = buttonArray.mexiqueButton = buttonArray.norvegeButton = buttonArray.VietnamButton = buttonArray.greceButton = buttonArray.moyenOrientButton = buttonArray.espagneDButton = false;
+        io.emit('japonSong');
       break;
 
       case 10:
         console.log('pays'+pinNum);
-        io.emit('songGrece');
-        return buttonArray.greceButton = true,
-        buttonArray.espagneButton = buttonArray.indeButton = buttonArray.danemarkButton = buttonArray.franceButton = buttonArray.royaumeUniButton = buttonArray.portugalButton = buttonArray.paysBasButton = buttonArray.canadaButton = buttonArray.mexiqueButton = buttonArray.norvegeButton = buttonArray.VietnamButton = buttonArray.japonButton = buttonArray.moyenOrientButton = buttonArray.espagneDButton = false;
+        io.emit('greceSong');
       break;
 
       case 11:
         console.log('pays'+pinNum);
-        io.emit('songVietnam');
-        return buttonArray.vietnamButton = true,
-        buttonArray.espagneButton = buttonArray.indeButton = buttonArray.danemarkButton = buttonArray.franceButton = buttonArray.royaumeUniButton = buttonArray.portugalButton = buttonArray.paysBasButton = buttonArray.canadaButton = buttonArray.mexiqueButton = buttonArray.norvegeButton = buttonArray.japonButton = buttonArray.greceButton = buttonArray.moyenOrientButton = buttonArray.espagneDButton = false;
+        io.emit('vietnamSong');
       break;
 
       case 12:
         console.log('pays'+pinNum);
-        io.emit('songNorvege');
-        return buttonArray.norvegeButton = true,
-        buttonArray.espagneButton = buttonArray.indeButton = buttonArray.danemarkButton = buttonArray.franceButton = buttonArray.royaumeUniButton = buttonArray.portugalButton = buttonArray.paysBasButton = buttonArray.canadaButton = buttonArray.mexiqueButton = buttonArray.VietnamButton = buttonArray.japonButton = buttonArray.greceButton = buttonArray.moyenOrientButton = buttonArray.espagneDButton = false;
+        io.emit('norvegeSong');
       break;
     };
   });
